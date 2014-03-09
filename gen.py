958cf06ccf6f8c9817c6931ebed4c15128efda9e
Will Haldean Brown <will.h.brown@gmail.com>
2014-03-08 16:10:29 -0800

    Generate dump to feed into generator.

--- gen.py
+++ gen.py
@@ -1,42 +1,84 @@
 import midi
+import pickle
+import pprint
 import sys
 
-def merge_tracks(track1, track2):
+class ev(object):
+  def __init__(self, tick, off_tick=None, note=None, velocity=None, tempo=None):
+    self.tick = tick
+    self.off_tick = off_tick
+    self.note = note
+    self.velocity = velocity
+    self.tempo = tempo
+
+  @property
+  def is_tempo(self):
+    return self.tempo is not None
+
+def track_to_ev(track):
+  evs = []
+  for i in range(len(track)):
+    n = track[i]
+    if isinstance(n, midi.NoteOnEvent):
+      tick = n.tick
+      note_val = n.data[0]
+      velocity = n.data[1]
+      off_tick = 0
+      for j in range(i+1, len(track)):
+        n_ = track[j]
+        off_tick += n_.tick
+        if isinstance(n_, midi.NoteOffEvent):
+          if n_.data[0] == note_val:
+            break
+      evs.append(ev(n.tick, off_tick, note=note_val, velocity=velocity))
+    if isinstance(n, midi.SetTempoEvent):
+      tempo = 0
+      for d in n.data:
+        tempo = tempo << 8 | d
+      evs.append(ev(n.tick, tempo=tempo))
+  return evs
+
+def ev_times(evs):
+  t = 0
+  res = []
+  for ev in evs:
+    res.append((t, ev))
+    t += ev.tick
+  return res
+
+def merge_evs(tracks):
   merged = []
-  i1, i2 = 0, 0
-  next_event_1, next_event_2 = track1[0].tick, track2[0].tick
-
-  while i1 < len(track1) and i2 < len(track2):
-    if next_event_1 < next_event_2:
-      forward_by = track1[i1].tick
-      track1[i1].tick = next_event_1
-      merged.append(track1[i1])
-
-      next_event_1 = track1[i1+1].tick
-      next_event_2 -= forward_by
-      i1 += 1
-
-    else:
-      forward_by = track2[i2].tick
-      track2[i2].tick = next_event_2
-      merged.append(track2[i2])
-
-      next_event_1 -= forward_by
-      next_event_2 = track2[i2+1].tick
-      i2 += 1
-
-  if i1 < len(track1):
-    merged.extend(track1[i1:])
-  elif i2 < len(track2):
-    merged.extend(track2[i2:])
+  for t in tracks:
+    merged.extend(t)
+  merged = sorted(merged)
+  last_t = 0
+  for ev in merged:
+    ev[1].tick = ev[0] - last_t
+    last_t = ev[0]
   return merged
 
+def dump(merged_evs):
+  filtered = []
+  last_t = 0
+  for t, ev in merged_evs:
+    if ev.is_tempo:
+      continue
+    filtered.append((t - last_t, ev.off_tick, ev.note, ev.velocity))
+    last_t = t
+  return filtered
+
 def main(argv):
-  if len(argv) < 2:
-    print 'usage: gen.py file.mid'
+  if len(argv) < 4:
+    print 'usage: gen.py file.mid tracks dump_to'
     return
   mid = midi.read_midifile(argv[1])
-  print mid
+  tracks_to_keep = map(int, argv[2].split(','))
+  tracks = []
+  for t in tracks_to_keep:
+    tracks.append(mid[t])
+  tracks = merge_evs(map(ev_times, map(track_to_ev, tracks)))
+  with open(argv[3], 'w') as f:
+    pickle.dump(dump(tracks), f)
 
 if __name__ == '__main__':
   main(sys.argv)
